d5a8e7b6e833c831e2b2672f5d5bae9be5e066a1
lx-dev-pm <lx-dev@pm.me>
2020-12-15 16:41:27 +0100

    refactor(main): refactor methods handle_connections

--- vachettp.v
+++ vachettp.v
@@ -9,26 +9,38 @@ pub fn run_app() {
 	println('Starting Vachettp on 8080...')
 	for {
 		mut conn := l.accept() or { panic('accept() failed') }
-		handle_connection_request(mut conn)
+		handle_connection(mut conn)
 	}
 }
 
-fn handle_connection_request(mut conn net.TcpConn) {
+fn handle_connection(mut conn net.TcpConn) {
 	conn.set_read_timeout(1 * time.second)
 	defer { conn.close() or {} }
 
+	request := handle_connection_request(conn)
+
+	println(request)
+	// Do something here with request (like handle routes)
+
+	handle_connection_response(conn, 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, World!')
+}
+
+/**
+* Handle the content of client
+**/
+fn handle_connection_request(conn net.TcpConn) Request {
 	mut reader := io.new_buffered_reader(reader: io.make_reader(conn))
 	
 	mut command := io.read_all(reader) or { []byte{} }
 
 	rp := RequestParser{command}
-	r := rp.parse()
 
-	println(r)
-
-	handle_connection_response(conn, 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, World!')
+	return rp.parse()
 }
 
+/**
+* Handle the response from server
+**/
 fn handle_connection_response(conn net.TcpConn, s string) {
 	conn.write(s.bytes())
 }
